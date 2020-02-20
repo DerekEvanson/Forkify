0572c69b511275c7d0da1fc438eb97855afac513
Derek Evanson <evanson.derek@gmail.com>
2020-02-19 18:38:07 -0600

    2C

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/*
+Global state of the app
+Search object
+Current recipe object
+Shopping list object
+liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+    //console.log(query);
+
+    if (query) {
+        // 2) New search object and add to state 
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+

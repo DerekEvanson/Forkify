4687587d88df92ce066bbaa9cf06294629163048
Derek Evanson <evanson.derek@gmail.com>
2020-02-20 22:21:53 -0600

    Format + Recipe Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import { elements, renderLoader, clearLoader } from './views/base';
+import Search from "./models/Search";
+import Recipe from "./models/Recipe";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /*
 Global state of the app
@@ -11,40 +12,81 @@ liked recipes
  */
 const state = {};
 
+/**
+ *  Search Controler
+ */
 const controlSearch = async () => {
-    // 1) Get query from view
-    const query = searchView.getInput();
-    //console.log(query);
+  // 1) Get query from view
+  const query = searchView.getInput();
+  //console.log(query);
 
-    if (query) {
-        // 2) New search object and add to state 
-        state.search = new Search(query);
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
 
-        // 3) Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
-        renderLoader(elements.searchRes);
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+      // 5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      alert("Error processing your search.");
+      clearLoader();
     }
-}
+  }
+};
 
-elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
 });
 
-elements.searchResPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline');
-    if (btn) {
-        const goToPage = parseInt(btn.dataset.goto, 10);
-        searchView.clearResults();
-        searchView.renderResults(state.search.result,goToPage);
-    }
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
 
+/**
+ *  Recipe Controler
+ */
+const controlRecipe = async () => {
+  // Get IF from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      console.log(state.recipe);
+    } catch (error) {
+      alert("Error processing recipe.");
+    }
+  }
+};
+
+window.addEventListener("hashchange", controlRecipe);
+window.addEventListener("load", controlRecipe);
+// alt write ['hashchange', 'load;'].forEach(e => window.addEventListener(event, controlRecipe));
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,29 +1,29 @@
 // Used to get Dom elements
 
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    searchRes: document.querySelector('.results'),
-    searchResList: document.querySelector('.results__list'),
-    searchResPages: document.querySelector('.results__pages')
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchRes: document.querySelector(".results"),
+  searchResList: document.querySelector(".results__list"),
+  searchResPages: document.querySelector(".results__pages")
 };
 
 export const elementStrings = {
-    loader: 'loader'
+  loader: "loader"
 };
 
 export const renderLoader = parent => {
-    const loader = `
+  const loader = `
         <div class="${elementStrings.loader}">
          <svg>
                 <use href="img/icons.svg#icon-cw"></use>
          </svg>
         </div>
     `;
-    parent.insertAdjacentHTML('afterbegin', loader);
+  parent.insertAdjacentHTML("afterbegin", loader);
 };
 
 export const clearLoader = () => {
-    const loader = document.querySelector(`.${elementStrings.loader}`);
-    if (loader) loader.parentElement.removeChild(loader);
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
 };
